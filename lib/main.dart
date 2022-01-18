99f72fdbcb3627134e2dde9b0e0d6e881d81e9cf
IbouKa <ibk2gallo@gmail.com>
2022-01-18 15:30:06 +0000

    Finish chapiter one and two

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,129 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(const MyApp());
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  String tap = 'Bonjour';
-  void tapOne(String x) {
-    setState(() {
-      tap = x;
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AppBar Widget',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('AppBar Widget'),
-          backgroundColor: Colors.deepPurple,
-          titleSpacing: 0.0,
-          centerTitle: true,
-          toolbarHeight: 50,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(180),
-            side: const BorderSide(width: 2, color: Colors.yellow),
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.notifications),
-              tooltip: 'Notification',
-              onPressed: () => {tapOne('Notification')},
-            ),
-            IconButton(
-              icon: const Icon(Icons.settings),
-              tooltip: 'Parametre',
-              onPressed: () => {tapOne('Parametre')},
-            ),
-            IconButton(
-              padding: const EdgeInsets.only(right: 20),
-              icon: const Icon(Icons.share),
-              tooltip: 'Partage',
-              onPressed: () => {tapOne('Partage')},
-            ),
-          ],
-          leading: IconButton(
-            icon: const Icon(Icons.menu),
-            tooltip: 'Menu',
-            onPressed: () => {tapOne('Menu')},
-          ),
-        ),
-        body: Center(
-          child: Text(
-            tap,
-            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      title: 'Container Widget',
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.deepOrange,
+        title: Text('Widget Scaffold'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to the body of the scafforld widget, I am Ibou Ka and i am a begginer in flutter dev',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 1,
+        child: Icon(Icons.style),
+        backgroundColor: Colors.deepOrangeAccent,
+        hoverColor: Colors.black,
+        focusColor: Colors.pink,
+        onPressed: () => {},
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.amber.shade50,
+        child: ListView(
+          children: const <Widget>[
+            DrawerHeader(
+              child: Icon(
+                Icons.money_sharp,
+                color: Colors.red,
+              ),
+              decoration: BoxDecoration(color: Colors.black),
+            ),
+            ListTile(
+              leading: Icon(Icons.list),
+              title: Text(
+                'Mes Tontines',
+                style: TextStyle(color: Colors.black, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person_off_outlined),
+              title: Text(
+                'Mon profil',
+                style: TextStyle(color: Colors.black, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text(
+                'Logout',
+                style: TextStyle(color: Colors.black, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.help),
+              title: Text(
+                'Help',
+                style: TextStyle(color: Colors.black, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              tileColor: Colors.deepOrange,
+              title: Text('A propos'),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black87,
+        currentIndex: 0,
+        fixedColor: Colors.blue,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Colors.deepOrange,
+            title: Text(''),
+            icon: const Icon(
+              Icons.home,
+              color: Colors.deepOrange,
+              size: 40,
+            ),
+          ),
+          BottomNavigationBarItem(
+            title: Text(''),
+            icon: Icon(
+              Icons.search,
+              color: Colors.deepOrange,
+              size: 40,
+            ),
+          ),
+          const BottomNavigationBarItem(
+            title: Text(''),
+            icon: Icon(
+              Icons.account_circle_rounded,
+              color: Colors.deepOrange,
+              size: 40,
+            ),
+          )
+        ],
+        onTap: (int indexOfItem) => {},
+      ),
+      drawerScrimColor: Colors.deepOrange,
     );
   }
 }
