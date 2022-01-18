8b15e4d22d3ebe1292d36c734924af395b2b0e5a
IbouKa <ibk2gallo@gmail.com>
2022-01-18 15:20:16 +0000

    Finish chapiter one and two

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,71 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'MaterialApp Widget',
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text('MaterialApp Widget'),
+  runApp(const HomePage());
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String tap = 'Bonjour';
+  void tapOne(String x) {
+    setState(() {
+      tap = x;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AppBar Widget',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('AppBar Widget'),
+          backgroundColor: Colors.deepPurple,
+          titleSpacing: 0.0,
+          centerTitle: true,
+          toolbarHeight: 50,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(180),
+            side: const BorderSide(width: 2, color: Colors.yellow),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.notifications),
+              tooltip: 'Notification',
+              onPressed: () => {tapOne('Notification')},
+            ),
+            IconButton(
+              icon: const Icon(Icons.settings),
+              tooltip: 'Parametre',
+              onPressed: () => {tapOne('Parametre')},
+            ),
+            IconButton(
+              padding: const EdgeInsets.only(right: 20),
+              icon: const Icon(Icons.share),
+              tooltip: 'Partage',
+              onPressed: () => {tapOne('Partage')},
+            ),
+          ],
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            tooltip: 'Menu',
+            onPressed: () => {tapOne('Menu')},
+          ),
+        ),
+        body: Center(
+          child: Text(
+            tap,
+            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
-      body: Container(),
-    ),
-    debugShowCheckedModeBanner: true,
-    showSemanticsDebugger: false,
-    debugShowMaterialGrid: false,
-    darkTheme: ThemeData.dark(),
-    themeMode: ThemeMode.dark,
-    theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-        visualDensity: const VisualDensity(horizontal: 0.8, vertical: 1)),
-  ));
+    );
+  }
 }
